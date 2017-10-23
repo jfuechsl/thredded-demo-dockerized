1b8066179e73e9385658d63c9d528a8dc6e130a6
Johann Fuechsl <johann@fuechsl.co>
2017-10-23 16:44:10 +0200

    Limit field lengths for indexed columens

--- db/schema.rb
+++ db/schema.rb
@@ -279,9 +279,9 @@ ActiveRecord::Schema.define(version: 20170510185425) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: "", null: false, limit: 191
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.string "reset_password_token", limit: 191
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
